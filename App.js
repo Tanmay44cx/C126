1558b7e01c6189930c2769845d22724203baa368
Tanmay44cx <67692638+Tanmay44cx@users.noreply.github.com>
2022-08-16 15:44:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import react from "react"
+import React from "react"
+import { Platform } from "react-native"
+import { View } from "react-native-web"
+import PickImage from "./screens/camera"
+
+export default class App extends React.Component{
+ 
+}
